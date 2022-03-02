de5ab6006b38bf5f19d2d60034842ad376b627ac
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2022-03-02 13:46:36 -0800

    Stopwatch JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+ useEffect(() => {
+   let interval = null;
+
+   start ? interval = setInterval(() => { setTime(prevTime => prevTime + 10) }, 10) : clearInterval(interval);
+
+   return () => clearInterval(interval);
+ }, [start]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1 id='stpWatch'>STOPWATCH</h1>
+      <div id='times'>
+      <span id='time1'>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+      <span id='time2'>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+      <span id='time3'>{("0" + Math.floor((time / 10) % 1000)).slice(-2)}</span>
+      </div>
+
+      <div id='buttons'>
+      <button onClick={() => setStart(true)} id='btnstart'>Start</button>
+      <button onClick={() => setStart(false)} id='btnstop'>Stop</button>
+      <button onClick={() => {setStart(false); setTime(0); }} id='btnreset'>Reset</button>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
